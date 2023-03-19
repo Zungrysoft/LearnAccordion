9b8b0e7b846f00232f22905b5bf5f39845294bac
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-03-19 00:15:46 -0700

    Dynamic icons

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -1,15 +1,25 @@
 import '../App.css';
-import { ReactComponent as YourSvg } from '../icon/technical.svg';
+import React, {useState, useEffect} from 'react';
 import settingsData from '../data/settings.json';
+import typeData from '../data/types.json';
 
 function Lesson({ lesson, data, onChange }) {
     const xPos = (lesson.x *45) + 50
     const yPos = lesson.y * 7 * settingsData.vertical_spacing
     const iconWidth = settingsData.icon_width * settingsData.icon_scale
     const iconMargin = (settingsData.icon_width - iconWidth) / 2
+
     return (
         <div className="bounding-box-lesson" style={{left: xPos+"vw", top: yPos+"vw"}}>
-            <YourSvg fill="currentColor" className="lesson-icon" style={{width: iconWidth+"vw", margin: iconMargin+"vw"}}/>
+            <img
+                src={`${process.env.PUBLIC_URL}/icon/${typeData[lesson.type].icon}.png`}
+                fill="currentColor"
+                className="lesson-icon"
+                style={{
+                    width: iconWidth+"vw",
+                    margin: iconMargin+"vw"
+                }}
+            />
         </div>
     )
 }
