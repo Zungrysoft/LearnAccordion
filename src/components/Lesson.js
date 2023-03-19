5f21fa72fa036293d5117ac24e4a75abe50ab290
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-03-19 02:31:03 -0700

    Added color to icons

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -5,18 +5,28 @@ import typeData from '../data/types.json';
 
 function Lesson({ lesson, data, onChange }) {
     const xPos = (lesson.x *45) + 50
-    const yPos = lesson.y * 7 * settingsData.vertical_spacing
+    const yPos = lesson.y * settingsData.icon_width * settingsData.vertical_spacing
     const iconWidth = settingsData.icon_width * settingsData.icon_scale
     const iconMargin = (settingsData.icon_width - iconWidth) / 2
 
+    let backgroundColor = settingsData.locked_color
+    // if (data && data.unlocked == true) {
+        backgroundColor = typeData[lesson.type].color
+    // }
+
     return (
-        <div className="bounding-box-lesson" style={{left: xPos+"vw", top: yPos+"vw"}}>
+        <div className="bounding-box-lesson" style={{
+            left: xPos+"vw",
+            top: yPos+"vw",
+            backgroundColor: backgroundColor,
+        }}>
             <img
                 src={`${process.env.PUBLIC_URL}/icon/${typeData[lesson.type].icon}.png`}
                 fill="currentColor"
                 className="lesson-icon"
                 style={{
                     width: iconWidth+"vw",
+                    height: iconWidth+"vw",
                     margin: iconMargin+"vw"
                 }}
             />
